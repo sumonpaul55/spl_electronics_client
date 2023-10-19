dd891333109a35dc7654bd89bcab515343b40088
sumon paul <sumpaul100@gmail.com>
2023-10-19 10:33:15 +0600

    navbar and home page product showall categories btn

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,26 +1,33 @@
-import React, { } from 'react';
+import React, { useState } from 'react';
 import Banner from './banner/Banner';
 import Brands from './brands/Brands';
 import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
     const loadedData = useLoaderData();
-    const sixBrand = loadedData.slice(0, 6)
+    const [bsix, setBsix] = useState(6)
+    let sixData = loadedData.slice(0, bsix)
+    const allDataDisplay = () => {
+        setBsix(loadedData.length)
+    }
     return (
         <div>
             <Banner></Banner>
             <section className='py-10 md:py-20 bg-slate-100'>
                 <div className="container mx-auto px-1">
                     <h1 className="text-xl md:text-2xl lg:text-3xl font-bold">Discover Excellence in Electronics Brand</h1>
-                    <div className='grid grid-cols-1 md:grid-cols-3 gap-5 lg:gap-16 mt-20'>
+                    <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5 lg:gap-16 mt-20'>
                         {
-                            sixBrand.map((brand, idx) => (
+                            sixData.map((brand, idx) => (
                                 <Brands key={brand._id} brand={brand} intemNo={idx + 1}></Brands>
                             ))
                         }
                     </div>
+                    <div className='mt-10 text-center'>
+                        {loadedData.length > bsix && <button className='btn btn-secondary mx-auto' onClick={allDataDisplay}>View All Categories</button>}
+                    </div>
                 </div>
-            </section>
+            </section >
 
         </div >
     );
--- src/shared/header/Navbar.jsx
+++ src/shared/header/Navbar.jsx
@@ -19,13 +19,20 @@ const Navbar = () => {
                 Swal(`${err}`)
             })
     }
+    document.body.addEventListener("click", () => {
+        setToggleBtn(false)
+    })
+    const handleShowOptionUser = (e) => {
+        e.stopPropagation();
+        setToggleBtn(!toggleBtn)
+    }
     return (
 
         <nav className='bg-grya-100 shadow relative'>
             <div className="container mx-auto p-1">
                 <div className='flex items-center justify-between'>
                     <div className="logo">
-                        <Link to="/"><img src={logo} className='w-52' alt="SCP electronics" /></Link>
+                        <Link to="/"><img src={logo} className='w-40 md:w-52' alt="SCP electronics" /></Link>
                     </div>
                     <div className={`absolute bg-emerald-500 h-screen lg:h-auto lg:w-auto lg:static lg:flex-row lg:bg-transparent top-14 w-full flex-col items-center justify-center flex gap-7 duration-200 z-50 ${toggle ? "left-0" : "-left-full"}`}>
                         <NavLink className="font-bold" to="/" onClick={() => setToggle(!toggle)}>Home</NavLink>
@@ -33,16 +40,21 @@ const Navbar = () => {
                         <NavLink className="font-bold" to="/my-cart" onClick={() => setToggle(!toggle)}>My Cart</NavLink>
                         <NavLink className="font-bold" to="/login" onClick={() => setToggle(!toggle)}>Login</NavLink>
                     </div>
-                    <div className='flex items-center gap-1 relative cursor-pointer' onClick={() => setToggleBtn(!toggleBtn)}>
+                    <div className='flex items-center gap-1 relative cursor-pointer' onClick={handleShowOptionUser}>
                         {
                             user?.photoURL ? <img src={user.photoURL} alt="" referrerPolicy="no-referrer" className='w-8 rounded-full' /> : <span className='text-xl'><RxAvatar /></span>
                         }
                         {
                             !loading && user?.displayName ? <h3 className='text-lg font-bold'>{user.displayName}</h3> : <h3 className="text-lg font-bold">User</h3>
                         }
-                        <div className={`absolute p-5 bg-gray-300 mt-32 hover:bg-gray-400 ${toggleBtn ? "block" : "hidden"}`}>
+                        <div className={`absolute p-3 bg-gray-300 top-[130%] left-[50%] -translate-x-[50%] w-full  ${toggleBtn ? "block" : "hidden"}`}>
                             {
-                                user ? <button onClick={handleLogout}>Log Out</button> : <Link to="/login">Login</Link>}
+                                user ?
+                                    <div>
+                                        <button onClick={handleLogout} className='hover:btn-secondary w-full'>Log Out</button>
+                                        <Link to="/addBrand" className='hover:btn-secondary w-full my-2 block text-center'>Add Brands</Link>
+                                    </div>
+                                    : <Link to="/login">Login</Link>}
                         </div>
                     </div>
                     <div className='block lg:hidden'>
